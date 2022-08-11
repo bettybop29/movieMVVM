0046616b8269c039a7c7cb4ad83d8f9891992e91
bettybop29 <babyimoneter@gmail.com>
2022-08-11 16:26:10 +0700

    added list news and button for change news

--- app/src/main/java/com/example/moviemvvm/data/api/ArticleAppleInterface.kt
+++ app/src/main/java/com/example/moviemvvm/data/api/ArticleAppleInterface.kt
@@ -3,9 +3,10 @@ package com.example.moviemvvm.data.api
 import com.example.moviemvvm.data.vo.NewsAppleResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ArticleAppleInterface {
 
     @GET("v2/everything")
-    fun getNewsApple():Single<NewsAppleResponse>
+    fun getNewsApple(@Query("q")id:String):Single<NewsAppleResponse>
 }
--- app/src/main/java/com/example/moviemvvm/ui/ListNewsActivity.kt
+++ app/src/main/java/com/example/moviemvvm/ui/ListNewsActivity.kt
@@ -1,8 +1,11 @@
 package com.example.moviemvvm.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,16 +25,35 @@ class ListNewsActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.listNews.layoutManager = LinearLayoutManager(applicationContext)
 
-        viewModel.getListNews()
+        val button: Button = findViewById(R.id.btnNewsApple)
+        button.setOnClickListener {
+            Toast.makeText(this, "apple news", Toast.LENGTH_SHORT).show()
+//            val apple = println("apple")
+            viewModel.getListNews("apple")
+
+        }
+        val buttons: Button = findViewById(R.id.btnNewsTesla)
+        buttons.setOnClickListener {
+            Toast.makeText(this, "tesla news", Toast.LENGTH_SHORT).show()
+//            val apple = println("apple")
+            viewModel.getListNews("tesla")
+
+        }
+
+
+        viewModel.getListNews("tesla")
 
         setObserver()
+
+
 
     }
     private fun setObserver(){
         viewModel.getNews().observe(this, Observer {
-            Log.i("List news activity", "response = $it")
+            Log.d("List news activity", "response = $it")
             adapter = NewsAdapter(it.articles)
             binding.listNews.adapter = adapter
         })
+
     }
 }
